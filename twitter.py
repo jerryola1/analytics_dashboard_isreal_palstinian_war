3a4eef336597a2893f52e3d7a80460454d3ca675
Olagunju Jeremiah <31370994+jerryola1@users.noreply.github.com>
2023-10-25 23:13:21 +0100

    added new dataset

--- twitter.py
+++ twitter.py
@@ -5,7 +5,6 @@ import pandas as pd
 from dash.dependencies import Input, Output
 import plotly.express as px
 
-
 external_stylesheets = [
     'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css']
 
@@ -13,11 +12,10 @@ external_stylesheets = [
 # Load the data into a DataFrame
 file_path = 'Updated_Combined_BBNaijaAllStars_2023_Data.xlsx'  # Update this path
 df = pd.read_excel(file_path)
+data = pd.read_csv('bbnaijaCombined.csv')
 
 # Inspect the first few rows of the DataFrame
-# print(df.head(2))
-# print(df.head(5))
-print(df.columns)
+print(data.head())
 print(df.head())
 
 df.drop(['Binders', 'User_y'], axis=1, inplace=True)
@@ -53,7 +51,7 @@ app.index_string = '''
 # Define primary colors
 primary_colors = {'background': '#2171b5', 'text': '#fd8d3c'}
 
-unique_usernames = df['Username'].unique()
+unique_usernames = data['tweets_username'].unique()
 username_options = [{'label': username, 'value': username}
                     for username in unique_usernames]
 
@@ -82,15 +80,15 @@ app.layout = html.Div(className='container-fluid alert alert-primary', style={'b
                 options=username_options,
                 style={'minWidth': '200px', 'maxWidth': '250px'}
             ),
-            className='col-12 col-md-3 my-1',
-            style={'marginLeft': 'auto', 'marginRight': 'auto'}
+            className='col-12 col-md-4 my-1',
+            # style={'marginLeft': 'auto', 'marginRight': 'auto'}
         ),
         html.Div(dcc.Dropdown(
             id='time-dropdown',
             options=time_range_options,
             style={'minWidth': '200px', 'maxWidth': '250px'}
         ),
-            className='col-sm-3 my-1'
+            className='col-sm-3 col-md-4 my-1'
         ),
 
         html.Div(dcc.Dropdown(
@@ -98,27 +96,65 @@ app.layout = html.Div(className='container-fluid alert alert-primary', style={'b
             options=time_range_options,
             style={'minWidth': '200px', 'maxWidth': '250px'}
         ),
-            className='col-sm-3 my-1'
+            className='col-sm-3 col-md-4 my-1'
         ),
 
-        html.Div(dcc.Dropdown(
-            id='sentiment-dropdown',
-            options=time_range_options,
-            style={'minWidth': '200px', 'maxWidth': '250px'}
-        ),
-            className='col-sm-3 my-1'
-        ),
+        # html.Div(dcc.Dropdown(
+        #     id='sentiment-dropdown',
+        #     options=time_range_options,
+        #     style={'minWidth': '200px', 'maxWidth': '250px'}
+        # ),
+        #     className='col-sm-3 my-1'
+        # ),
     ]),
 
+
+
+    # # Overview Metrics Row 3
+    # html.Div(className='row py-3 mb-3 bg-warning shadow rounded', children=[
+    #     html.Div(id='total-tweets', className='col-4 text-center'),
+    #     html.Div(id='unique-contributors', className='col-4'),
+    #     html.Div(id='avg-likes', className='col-4')
+    #     # html.Div(id='avg-retweets', className='col-3')
+    # ]),
 
 
     # Overview Metrics Row 3
-    html.Div(className='row py-3 mb-3 bg-warning', children=[
-        html.Div(id='total-tweets', className='col-3 text-center'),
-        html.Div(id='unique-contributors', className='col-3'),
-        html.Div(id='avg-likes', className='col-3'),
-        html.Div(id='avg-retweets', className='col-3')
-    ]),
+    html.Div(
+        className='row py-3 mb-3 text-warning shadow-sm rounded',
+        children=[
+            html.Div(
+                id='total-tweets',
+                className='col text-center p-3 mb-5  rounded',
+                children=[
+                    # Your pie chart or other chart components go here
+                ]
+            ),
+            html.Div(
+                id='pie-chart',
+                className='col text-center p-3 mb-5 rounded',
+                children=[
+                    # Your pie chart or other chart components go here
+                ]
+            ),
+            html.Div(
+                id='pie-chart',
+                className='col text-center p-3 mb-5 rounded',
+                children=[
+                    dcc.Graph(
+                        id='pie-chart-graph',
+                        config={'staticPlot': False},
+                        figure=px.pie(
+                            data,
+                            names='tweets_username',
+                            title='Distribution of Tweets Among Users'
+                        )
+                    )
+                ]
+            )
+        ]
+    ),
+
 
 
     # # Tweet Insights Row 4
@@ -140,7 +176,7 @@ app.layout = html.Div(className='container-fluid alert alert-primary', style={'b
             html.Div([dcc.Graph(
                 id='engagement-metrics')],
                 className='col-6 mb-3'),
-            
+
             html.Div([dcc.Graph(id='time-analysis')], className='col-6 mb-3')
         ]),
 
@@ -167,20 +203,45 @@ app.layout = html.Div(className='container-fluid alert alert-primary', style={'b
 )
 def update_total_tweets(selected_username):
     if selected_username:
-        total_tweets = df[df['Username'] == selected_username].shape[0]
-        return f"Total Tweets: {total_tweets}"
+        total_tweets = data[data['tweets_username']
+                            == selected_username].shape[0]
+        return html.B(f"Total Tweets: {total_tweets}")
     else:
-        return "Total Tweets: N/A"
-    
-    
+        return html.B("Total Tweets: N/A")
+
+
 @app.callback(
     Output('engagement-metrics', 'figure'),
     Input('username-dropdown', 'value')
 )
 def update_graph(selected_username):
-    filtered_df = df[df['Username'] == selected_username]
-    fig = px.bar(filtered_df, x='UTC Date', y='Likes count')
+    filtered_df = data[data['tweets_username'] == selected_username]
+    fig = px.bar(filtered_df, x='tweets_utc_date', y='tweets_likes_count')
     return fig
+
+
+@app.callback(
+    Output('pie-chart-graph', 'figure'),
+    [Input('username-dropdown', 'value')]
+)
+def update_pie_chart(selected_username):
+    if selected_username:
+        # Filter data for selected username
+        filtered_df = data[data['tweets_username'] == selected_username]
+        
+        # Aggregate data (e.g., count tweets per username)
+        agg_data = filtered_df.groupby('tweets_username').size().reset_index(name='tweet_count')
+        
+        # Create pie chart figure
+        fig = px.pie(
+            agg_data, 
+            names='tweets_username', 
+            values='tweet_count',
+            title=f'Distribution of Tweets for {selected_username}'
+        )
+        return fig
+    else:
+        return dash.no_update
 
 
 
